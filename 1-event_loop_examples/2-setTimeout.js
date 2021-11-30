7044bcb1a8ba7d64c6107ec7f27650438f9da9ad
sushantdesaur <desaursushant@gmail.com>
2021-11-30 14:23:07 +0530

    used setTimeout function

---
+++ 1-event_loop_examples/2-setTimeout.js
@@ -0,0 +1,13 @@
+// Started a operating system process
+
+console.log("First Task");
+
+
+setTimeout(()=>{
+    console.log("Second task");
+}, 10);
+
+console.log("Next Task");
+
+// Completed and exited operating system process
+
